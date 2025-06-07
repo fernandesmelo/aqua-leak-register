a450315228167ae38313493cb4aa709b7161523e
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2025-06-07 09:10:47 -0300

    atualiza string de conexão do MongoDB para usar a versão em nuvem

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/pets', petRouter);
 
-mongoose.connect('mongodb://localhost:27017/plantation', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://laerciofernandesmelonetoo:F4htN3zIPWLbOVUm@petregisterapp.ehb4tjy.mongodb.net/?retryWrites=true&w=majority&appName=petregisterapp', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     app.listen(3000, () => {
       console.log('Servidor rodando na porta 3000');
