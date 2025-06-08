f8abc7899f3179a3272d5a1011d19314ae8dcb4d
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2025-06-08 19:21:20 -0300

    Atualiza o nome e slug do aplicativo para "aqua-leak-register" e remove a imagem de splash "FindPetSplash.png"

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const multer = require('multer');
-const path = require('path');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const multer = require("multer");
+const path = require("path");
+require("dotenv").config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('/uploads', express.static('uploads'));
+app.use("/uploads", express.static("uploads"));
 
 const mongoURI = process.env.MONGO_URI;
 
@@ -17,51 +17,53 @@ const mongoURI = process.env.MONGO_URI;
   try {
     await mongoose.connect(mongoURI, {
       useNewUrlParser: true,
-      useUnifiedTopology: true
+      useUnifiedTopology: true,
     });
-    console.log('Conectado ao MongoDB com sucesso!');
+    console.log("Conectado ao MongoDB com sucesso!");
   } catch (error) {
-    console.error('Erro ao conectar no MongoDB:', error.message);
+    console.error("Erro ao conectar no MongoDB:", error.message);
   }
 })();
 
-const Pet = mongoose.model('Pet', {
+const Leak = mongoose.model("Leak", {
   name: String,
   description: String,
   photo: String,
   latitude: Number,
   longitude: Number,
-  emPerigo: Boolean
+  emPerigo: Boolean,
 });
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/');
+    cb(null, "uploads/");
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + path.extname(file.originalname));
-  }
+  },
 });
 const upload = multer({ storage: storage });
 
-app.get('/pets', async (req, res) => {
-  const pets = await Pet.find();
-  res.json(pets);
+app.get("/leaks", async (req, res) => {
+  const leaks = await Leak.find();
+  res.json(leaks);
 });
 
-app.post('/pets', upload.single('photo'), async (req, res) => {
+app.post("/leaks", upload.single("photo"), async (req, res) => {
   const { name, description, latitude, longitude, emPerigo } = req.body;
   const photo = req.file ? req.file.path.replace(/\\/g, "/") : null;
-  const pet = new Pet({
+  const leak = new Leak({
     name,
     description,
     photo,
     latitude,
     longitude,
-    emPerigo: emPerigo === 'true'
+    emPerigo: emPerigo === "true",
   });
-  await pet.save();
-  res.json(pet);
+  await leak.save();
+  res.json(leak);
 });
 
-app.listen(3000, '0.0.0.0', () => console.log('Backend listening on port 3000'));
+app.listen(3000, "0.0.0.0", () =>
+  console.log("Backend listening on port 3000")
+);
